c8fd3a4627b22abf49a47cdd7b1a5a78e10e7ab5
Shresthjaiswal2002 <shresthjaiswal2002@gmail.com>
2024-06-30 23:30:20 +0530

    Your commit message here

---
+++ server/server.js
@@ -0,0 +1,26 @@
+// server/server.js
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 5000;
+
+let leftClicks = 0;
+let rightClicks = 0;
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/clicks', (req, res) => {
+  const { direction } = req.body;
+  if (direction === 'left') {
+    leftClicks += 1;
+  } else if (direction === 'right') {
+    rightClicks += 1;
+  }
+  console.log(`Left Clicks: ${leftClicks}, Right Clicks: ${rightClicks}`);
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
